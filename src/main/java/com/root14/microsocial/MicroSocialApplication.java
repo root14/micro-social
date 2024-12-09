93a1f6b568f21139a525c4e57bdd652e8d317740
Basri İlkay GAVAZ <ilkaygavaz2@gmail.com>
2024-12-09 14:11:48 +0300

    initial commit

---
+++ src/main/java/com/root14/microsocial/MicroSocialApplication.java
@@ -0,0 +1,13 @@
+package com.root14.microsocial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroSocialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroSocialApplication.class, args);
+    }
+
+}
