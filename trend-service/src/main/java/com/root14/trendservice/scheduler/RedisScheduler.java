11a2ec4d9cb9c04d8dfac223994a8f71c7ec4ce2
Basri İlkay GAVAZ <ilkaygavaz2@gmail.com>
2025-01-03 14:11:32 +0300

    add diagram.png

--- trend-service/src/main/java/com/root14/trendservice/scheduler/RedisScheduler.java
+++ trend-service/src/main/java/com/root14/trendservice/scheduler/RedisScheduler.java
@@ -1,14 +1,9 @@
 package com.root14.trendservice.scheduler;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataAccessException;
-import org.springframework.data.redis.connection.RedisConnection;
-import org.springframework.data.redis.core.RedisCallback;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
-
-import java.util.Arrays;
 
 
 @Service
